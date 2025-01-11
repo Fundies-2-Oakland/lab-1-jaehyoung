b7a5e4a0e298393d3e3a4c845638c6d4e9cc89ef
Jaehyoung Jung <kevinj33307@gmail.com>
2025-01-11 00:57:50 -0800

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Vector3D testvector = new Vector3D((double)4.0F, (double)6.0F, (double)2.0F);
+        System.out.println(testvector);
+    }
+}
---
+++ Vector3D.java
@@ -0,0 +1,23 @@
+public class Vector3D {
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+}
